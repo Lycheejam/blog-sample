90c5ab2ce1a8baf9f66e2edc735c9781ead4aec8
Lycheejam <lycheejamwow@gmail.com>
2019-03-01 01:47:26 +0900

    add using mysql error project

---
+++ using-mysql-package/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace using_mysql_package.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
