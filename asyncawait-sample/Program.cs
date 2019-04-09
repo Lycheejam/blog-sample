9a6c106b87b6acfde23df8e8ce71e6aecf19a3ea
Lycheejam <lycheejamwow@gmail.com>
2019-04-09 11:55:42 +0900

    add async await sample project

---
+++ asyncawait-sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asyncawait_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
