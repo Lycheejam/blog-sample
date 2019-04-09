c5af1939ff545a4e79b250ce2d154d95af10e079
Lycheejam <lycheejamwow@gmail.com>
2019-04-09 17:23:05 +0900

    add async await sample code

---
+++ asyncawait-sample/Downloader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace asyncawait_sample {
+    public class Downloader {
+
+        public Downloader() { }
+
+        //同期メソッド
+        public string BlogDownload(string url) {
+            using (var client = new WebClient()) {
+                Console.WriteLine("Download Start url:{0}", url);
+                var text = client.DownloadString(url);
+
+                Console.WriteLine("Download Complete url:{0}", url);
+                Console.WriteLine("");
+
+                return text;
+            }
+        }
+
+        //非同期メソッド
+        public async Task<string> BlogDownloadAsync(string url) {
+            using (var client = new WebClient()) {
+                Console.WriteLine("Download Start url:{0}", url);
+                var text = await client.DownloadStringTaskAsync(url);
+
+                Console.WriteLine("Download Complete url:{0}", url);
+                Console.WriteLine("");
+
+                return text;
+            }
+        }
+    }
+}
--- asyncawait-sample/Program.cs
+++ asyncawait-sample/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace asyncawait_sample
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace asyncawait_sample {
+    class Program {
+        static async Task Main(string[] args) {
             Console.WriteLine("Hello World!");
+
+            var dlclient = new Downloader();
+
+            //同期メソッド
+            //dlclient.BlogDownload();
+            Console.WriteLine("同期メソッド");
+            dlclient.BlogDownload("https://kitigai.hatenablog.com/entry/2019/04/04/133432");
+            dlclient.BlogDownload("https://kitigai.hatenablog.com/entry/2019/04/03/155921");
+            dlclient.BlogDownload("https://kitigai.hatenablog.com/entry/2019/04/01/203347");
+
+            //非同期メソッド
+            Console.WriteLine("非同期メソッド");
+            var tasks = new Task<string>[] {
+                Task.Run(() => dlclient.BlogDownloadAsync("https://kitigai.hatenablog.com/entry/2019/04/04/133432")),
+                Task.Run(() => dlclient.BlogDownloadAsync("https://kitigai.hatenablog.com/entry/2019/04/03/155921")),
+                Task.Run(() => dlclient.BlogDownloadAsync("https://kitigai.hatenablog.com/entry/2019/04/01/203347"))
+            };
+
+            Console.WriteLine("この間に非同期メソッドが実行されてるよ。");
+
+            await Task.WhenAll(tasks);
         }
     }
 }
