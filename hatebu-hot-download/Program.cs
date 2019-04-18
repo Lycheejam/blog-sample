845934e397939bd45ff484703bb5bd86d75a5a31
Lycheejam <lycheejamwow@gmail.com>
2019-04-18 16:07:47 +0900

    add hatena bookmark hotentry download sample

---
+++ hatebu-hot-download/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hatebu_hot_download
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
