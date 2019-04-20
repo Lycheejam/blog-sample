e031a3b4494a94bc406149d7d842a02a5cf48bcf
Lycheejam <lycheejamwow@gmail.com>
2019-04-20 23:17:39 +0900

    add hatebu downloder

---
+++ hatebu-hot-download/Downloader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace hatebu_hot_download {
+    public class Downloader {
+        private static HttpClient client = new HttpClient();
+
+        public Downloader() {
+            client.DefaultRequestHeaders.Add(
+                "User-Agent",
+                "hogehoge"
+            );
+        }
+
+        public async Task<string> GetBodyAsync (string url) {
+            var response = await client.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        //public async Task<string> GetBodyAsync(string url) {
+        //    using (var client = new HttpClient()) {
+        //        var response = await client.GetStringAsync(url);
+        //        return response;
+        //    }
+        //}
+    }
+}
--- hatebu-hot-download/Program.cs
+++ hatebu-hot-download/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-namespace hatebu_hot_download
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace hatebu_hot_download {
+    class Program {
+        static async Task Main(string[] args) {
             Console.WriteLine("Hello World!");
+
+            var url = "http://b.hatena.ne.jp/hotentry/all";
+
+            var downloder = new Downloader();
+            Console.WriteLine(await downloder.GetBodyAsync(url));
         }
     }
 }
