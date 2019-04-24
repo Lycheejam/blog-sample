ed3e9472c127d265894e050e7c450c76e84a795d
Lycheejam <lycheejamwow@gmail.com>
2019-04-24 09:38:08 +0900

    add connection pool zikken project

---
+++ connection-zikken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace connection_zikken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
