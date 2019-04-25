8ca8d5bc240f8739fda7d38dbd3c43b3fd61e33f
Lycheejam <lycheejamwow@gmail.com>
2019-04-25 10:18:19 +0900

    Create HttpClientGoodExample.cs

---
+++ connection-zikken/HttpClientBadExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace connection_zikken {
+    public class HttpClientBadExample {
+        public async Task<string> RunAsync (string url) {
+            var result = "";
+            using (var client = new HttpClient()) {
+                var response = await client.GetAsync(url);
+                result = await response.Content.ReadAsStringAsync();
+            }
+            return result;
+        }
+    }
+}
