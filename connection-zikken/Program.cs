690c9092862cf497e19d807e632b46153b35a12b
Lycheejam <lycheejamwow@gmail.com>
2019-04-25 11:24:29 +0900

    実験プログラム

--- connection-zikken/Program.cs
+++ connection-zikken/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-namespace connection_zikken
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace connection_zikken {
+    class Program {
+        static async Task Main(string[] args) {
             Console.WriteLine("Hello World!");
+
+            var url = "http://todo.kitigai.org/zikken/";
+
+            //Console.WriteLine("悪い例");
+
+            //var badclient = new HttpClientBadExample();
+
+            //for (int i = 0; i < 10; i++) {
+            //    Console.WriteLine("{0}回目：{1}", i, await badclient.RunAsync(url));
+            //}
+
+            Console.WriteLine("良い例");
+
+            var goodclient = new HttpClientGoodExample();
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine("{0}回目：{1}", i, await goodclient.RunAsync(url));
+            }
         }
     }
 }
